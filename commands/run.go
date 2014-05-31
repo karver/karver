91150ed4a1c65d0e18e4de6088ddfb7bad2a68f6
Salvador Gironès <salvadorgirones@gmail.com>
2014-05-31 14:19:52 -0700

    foo

--- commands/run.go
+++ commands/run.go
@@ -1,6 +1,12 @@
 package commands
 
-import "github.com/codegangsta/cli"
+import (
+	"fmt"
+	"log"
+
+	"github.com/codegangsta/cli"
+	"github.com/karver/karver/migrations"
+)
 
 var Run = cli.Command{
 	Name:   "run",
@@ -9,5 +15,20 @@ var Run = cli.Command{
 }
 
 func runCmd(c *cli.Context) {
+	target := "/"
+	m := &migrations.Migration{
+		Timestamp: "foo",
+		Name:      "run",
+		Path:      "/Users/salvador/devel/golang/src/github.com/karver/karver/run",
+	}
+
+	stdout, stderr, err := m.Run(target)
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println(stdout)
+	fmt.Println(stderr)
 
 }
--- migrations/migrations.go
+++ migrations/migrations.go
@@ -1,25 +1,85 @@
 package migrations
 
 import (
+	"fmt"
+	"io"
 	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"regexp"
+	"syscall"
 )
 
 type Migration struct {
-	timestamp string
-	name      string
-	path      string
+	Timestamp string
+	Name      string
+	Path      string
 }
 
-func (m *Migration) Run(target string) (string, error) {
-	// cmd := exec.Command("ls", "-la")
-	// var out bytes.Buffer
-	// cmd.Stdout = &out
-	// err := cmd.Run()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("ls: %q\n", out.String())
+func (m *Migration) Run(target string) (string, string, error) {
+	// Copy migration to the target path
+	f, err := os.Open(m.Path)
+	if err != nil {
+		return "", "", fmt.Errorf("Error opening the file %s: %s", m.Path, err.Error())
+	}
+	defer f.Close()
+
+	tmpf, err := ioutil.TempFile(target, "")
+	if err != nil {
+		return "", "", fmt.Errorf("Error creating tmp file in %s: %s", target, err.Error())
+	}
+	defer tmpf.Close()
+
+	_, err = io.Copy(tmpf, f)
+	if err != nil {
+		return "", "", fmt.Errorf("Error copying the file %s: %s", m.Path, err.Error())
+	}
+
+	// Prepare the target environment
+	if err := syscall.Chroot(target); err != nil {
+		return "", "", fmt.Errorf("Error trying to chroot into %s when running migration %s: %s", target, m.Name, err.Error())
+	}
+
+	tmpf.Chmod(777)
+
+	// Run the migration
+	cmd := exec.Command(target + filepath.Base(tmpf.Name()))
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return "", "", err
+	}
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		return "", "", err
+	}
+
+	if err := cmd.Start(); err != nil {
+		return "", "", fmt.Errorf("Error starting migration %s: %s", target, m.Name, err.Error())
+	}
+
+	var (
+		sout string
+		serr string
+	)
+
+	if b, err := ioutil.ReadAll(stdout); err == nil {
+		sout = string(b)
+	}
+
+	if b, err := ioutil.ReadAll(stderr); err == nil {
+		serr = string(b)
+	}
+
+	log.Printf("Running migration %s...", m.Name)
+	cmd.Wait()
+
+	os.Remove(target + filepath.Base(tmpf.Name()))
+
+	return sout, serr, nil
 }
 
 func Run(target string) (*Migration, error) {
@@ -37,8 +97,8 @@ func List() ([]*Migration, error) {
 	for _, f := range files {
 		m := r.FindStringSubmatch(f.Name())
 		if len(m) > 0 && !f.IsDir() {
-			migration, err := Load(f.Name())
-			migrations := append(migrations, migration)
+			migration, _ := Load(f.Name())
+			migrations = append(migrations, migration)
 		}
 	}
 
